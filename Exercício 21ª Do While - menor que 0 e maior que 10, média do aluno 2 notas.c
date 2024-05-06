fda6f4253524039dcf568845dd228d75cacec671
Bitencourtgabriel <162353559+Bitencourtgabriel@users.noreply.github.com>
2024-05-06 12:49:59 -0300

    Update Exercício 21ª Do While - menor que 0 e maior que 10, média do aluno 2 notas.c

--- Exercício 21ª Do While - menor que 0 e maior que 10, média do aluno 2 notas.c
+++ Exercício 21ª Do While - menor que 0 e maior que 10, média do aluno 2 notas.c
@@ -5,33 +5,33 @@
 int main (){
 setlocale(LC_ALL,"");
 
-// Declarando as vari·veis.
+// Declarando as vari√°veis.
 
 float notas, soma = 0, media;
 int i;
-// Solicitando dados ao usu·rio.
+// Solicitando dados ao usu√°rio.
 
 for(i = 1; i < 2; i++){
 do{
 		
-	printf("Digite a %d™ nota: ",i);
+	printf("Digite a %d¬™ nota: ",i);
 	scanf("%f", &notas);
 
 	if(notas < 0 || notas > 10){
-		printf("Nota inv·lida. Digite uma nota entre 0 e 10.\n");
+		printf("Nota inv√°lida. Digite uma nota entre 0 e 10.\n");
 	}
 		
 }while(notas < 0 || notas > 10);
 soma += notas;
 }
 
-// C·lculo da media	
+// C√°lculo da media	
 	
 	media = soma / 2.0;
 
 // Exibindo resultado.
 printf("Soma: %i\n",soma);
-printf("A mÈdia È: %.2f\n",media);	
+printf("A m√©dia √©: %.2f\n",media);	
 	
 return 0;	
 }
